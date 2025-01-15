670fc6c48986ea3437a22197f35abfa4c40f3065
IPariz1983 <israel_pariz@hotmail.com>
2025-01-15 10:16:04 -0300

    Addfiles

---
+++ GitHubRepoApp/Controllers/GitHubController.cs
@@ -0,0 +1,49 @@
+﻿using GitHubRepoApp.Application.UseCases; // Importe os namespaces dos Use Cases
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitHubRepoApp.Controllers
+{
+    [Route("repos")]
+    [ApiController]
+    public class RepositoriesController : ControllerBase // Nome do controller mais descritivo
+    {
+        private readonly SyncRepositoriesUseCase _syncRepositoriesUseCase;
+        private readonly GetRepositoriesUseCase _getRepositoriesUseCase;
+
+        public RepositoriesController(SyncRepositoriesUseCase syncRepositoriesUseCase, GetRepositoriesUseCase getRepositoriesUseCase)
+        {
+            _syncRepositoriesUseCase = syncRepositoriesUseCase;
+            _getRepositoriesUseCase = getRepositoriesUseCase;
+        }
+
+        [HttpPost("sync/{username}")]
+        public async Task<IActionResult> SyncRepositories(string username)
+        {
+            try
+            {
+                await _syncRepositoriesUseCase.ExecuteAsync(username);
+                return Ok(); // Retorna 200 OK em caso de sucesso
+            }
+            catch (Exception ex)
+            {
+                // Log the exception here
+                return StatusCode(500, $"Internal Server Error: {ex.Message}"); // Retorna 500 com mensagem de erro
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRepositories(string username)
+        {
+            try
+            {
+                var repositories = await _getRepositoriesUseCase.ExecuteAsync(username);
+                return Ok(repositories);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception here
+                return StatusCode(500, $"Internal Server Error: {ex.Message}"); // Retorna 500 com mensagem de erro
+            }
+        }
+    }
+}
---
+++ GitHubRepoApp/Program.cs
@@ -0,0 +1,58 @@
+using GitHubRepoApp.Application.Mappings;
+using GitHubRepoApp.Application.UseCases;
+using GitHubRepoApp.Domain.Interfaces;
+using GitHubRepoApp.Infrastructure;
+using GitHubRepoApp.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configuração de logging com Serilog
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+// Configuração de serviços
+
+builder.Services.AddHttpClient();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configuração do banco de dados PostgreSQL
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+
+
+// Configuração de dependências e AutoMapper
+builder.Services.AddScoped<IGitHubService, GitHubService>();
+builder.Services.AddScoped<SyncRepositoriesUseCase>();
+builder.Services.AddScoped<GetRepositoriesUseCase>();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+builder.Services.AddInfrastructure();
+
+
+
+var app = builder.Build();
+
+// Configuração do middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
